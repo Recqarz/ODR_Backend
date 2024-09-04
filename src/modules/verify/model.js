4fab7a89c3d14fa654111673da12f140ca0c3b1d
aniveshAreness <anivesh.bhadauriya@recqarz.com>
2024-09-04 17:07:58 +0530

    adding verify email

---
+++ src/modules/verify/model.js
@@ -0,0 +1,27 @@
+
+import mongoose  from "mongoose";
+
+const verifyEmail = mongoose.Schema({
+
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.']
+      },
+    isVerified:{
+        type: Boolean,
+        required: true,
+        default:false
+    },
+    token:{
+        type:String,
+    },
+    tokenValid:{
+        type:Date,
+    }
+})
+
+const emailVerify = mongoose.Schema("emailverify", verifyEmail)
+export default emailVerify
